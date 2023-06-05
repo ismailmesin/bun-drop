509d57170aa398ca6116ed7e1fb19901d735bed5
ismailmesin <113332232+ismailmesin@users.noreply.github.com>
2023-06-05 14:16:40 +0200

    fixed the logo-image path in the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {ShoppingCart} from 'phosphor-react';
-import logo from '../images/logo.png'
+import logo from '../Logo/logo.png'
 
 
 function Navbar() {
