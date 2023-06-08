e9b68ae7ce081ff49516a364cb723fcbd4f344ae
ismailmesin <113332232+ismailmesin@users.noreply.github.com>
2023-06-09 00:49:36 +0200

    finished project

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
 import { useEffect, useState } from 'react';
 import CartItem from './CartItem';
 import { Link } from 'react-router-dom';
 
 function Cart() {
-
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
@@ -23,27 +22,34 @@ function Cart() {
     }
   };
 
+  const isCheckoutDisabled = cartItems.every(item => item.amount === 0);
 
   return (
     <div>
       <div className='menu-container flex-container'>
-          {cartItems?.map((m) => (
-            <CartItem
-              key={m.id}
-              name={m.name}
-              img={m.img}
-              price={m.price}
-              id={m.id}
-              amount={m.amount}
-            />
-          ))}
+        {cartItems?.map((m) => (
+          <CartItem
+            key={m.id}
+            name={m.name}
+            img={m.img}
+            price={m.price}
+            id={m.id}
+            amount={m.amount}
+          />
+        ))}
       </div>
       <div className='flex-container'>
-        <Link to='/menu' className='light-grey'><button className='cart-button light-grey'>Continue Shopping</button></Link>
-        <Link to='/checkout' className='light-grey'><button className='cart-button light-grey'>Checkout</button></Link>
+        <Link to='/menu' className='light-grey'>
+          <button className='cart-button light-grey'>Continue Shopping</button>
+        </Link>
+        <Link to='/checkout' className='light-grey'>
+          <button className='cart-button light-grey' disabled={isCheckoutDisabled}>
+            Checkout
+          </button>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
